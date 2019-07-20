58723b0e97576df3f1e2647faa443946b96293b3
Seungdols <seungdols0822@gmail.com>
2019-07-20 16:31:06 +0900

    Add sample project

---
+++ app/src/main/kotlin/hello.kt
@@ -0,0 +1,15 @@
+/**
+ *
+ * @PACKAGE
+ * @AUTHOR  seungdols
+ * @DATE    2019-07-20
+ */
+/**
+ * @PACKAGE
+ * @Author seungdols
+ * @Date 2019-07-13
+ */
+
+fun main(args: Array<String>) {
+    hello()
+}
---
+++ lib/src/main/kotlin/hello.kt
@@ -0,0 +1,9 @@
+/**
+ * @PACKAGE
+ * @Author seungdols
+ * @Date 2019-07-13
+ */
+
+fun hello() {
+    println("Hello")
+}
